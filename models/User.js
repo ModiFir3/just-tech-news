5c42e72d2732dde84e7e288a69b41022bd0e41f8
PaulS <psoliz333@gmail.com>
2022-04-26 17:26:43 -0500

    added bcrypt to password and ability to change password and still have it bcrypted

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 //create our User model
 class User extends Model {}
@@ -47,8 +48,19 @@ User.init(
         }
     },
     {
+        hooks: {
+            //set up beforeCreate lifecycle 'hook' functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await  bcrypt.hash(newUserData.password, 10);
+                return newUserData
+            },
+            //set up beforeUpdate lifecycle 'hook' functionality
+            async beforeUpdate(updateUserData) {
+                updateUserData.password = await bcrypt.hash(updateUserData.password, 10);
+                return updateUserData;
+            }
+        },
         //TBALE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
         //pass in our imported sequelize connection (the direct connection to our database)
         sequelize,
         //don't automatically create createAt/updateAt timestamp fields
