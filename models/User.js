328a09fbca2efbd0c930701777c7ee0e012063ad
PaulS <psoliz333@gmail.com>
2022-04-26 18:08:36 -0500

    created login api with ablility to check password from bcrypt

--- models/User.js
+++ models/User.js
@@ -3,7 +3,12 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 //create our User model
-class User extends Model {}
+class User extends Model {
+    //set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 //define table colums and configuration
 User.init(
@@ -51,7 +56,7 @@ User.init(
         hooks: {
             //set up beforeCreate lifecycle 'hook' functionality
             async beforeCreate(newUserData) {
-                newUserData.password = await  bcrypt.hash(newUserData.password, 10);
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
                 return newUserData
             },
             //set up beforeUpdate lifecycle 'hook' functionality
