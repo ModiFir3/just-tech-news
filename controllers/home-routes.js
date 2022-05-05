ce0438a5d8196c8c215fc1fe6e33c36be023854d
PaulS <psoliz333@gmail.com>
2022-05-04 22:19:22 -0500

    created user ability to upvote when signed in

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models')
+const { Post, User, Comment, Vote } = require('../models')
 
+// get all posts for homepage
 router.get('/', (req, res) => {
-    console.log(req.session);
-
+    console.log('======================');
     Post.findAll({
         attributes: [
             'id',
@@ -16,24 +16,27 @@ router.get('/', (req, res) => {
         include: [
             {
                 model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'created_at'],
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
                 include: {
                     model: User,
                     attributes: ['username']
                 }
+            },
+            {
+                model: User,
+                attributes: ['username']
             }
         ]
     })
         .then(dbPostData => {
             const posts = dbPostData.map(post => post.get({ plain: true }));
-            //pass a single post object into the homepage template
-            console.log(dbPostData[0]);
+
             res.render('homepage', { posts });
         })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
-        })
+        });
 });
 
 router.get('/login', (req, res) => {
@@ -43,6 +46,51 @@ router.get('/login', (req, res) => {
     }
 
     res.render('login');
-})
+});
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+
+            // serialize the data
+            const post = dbPostData.get({ plain: true });
+
+            // pass data to template
+            res.render('single-post', { post });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;
